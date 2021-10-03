46076dbaa88b7903a7d52681c0d329531bf284bc
Kaisei Waki <me@kaisei.dev>
2021-10-04 08:40:00 +0900

    Updates

---
+++ component---src-pages-index-js-d88dc7780f8302edf87b.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkember_site=self.webpackChunkember_site||[]).push([[678],{7704:function(e,l,t){t.r(l);var n=t(7294);l.default=function(){return n.createElement("main",null,n.createElement("title",null,"Ember Theme"),n.createElement("h1",null,'This is a color palette called "Ember."'),n.createElement("div",null,n.createElement("h2",null," Colors "),n.createElement("div",null)),n.createElement("p",null,"This color palette is in development."))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-d88dc7780f8302edf87b.js.map
