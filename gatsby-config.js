6df66afc4fbe67f50d5a28714db6032af852de37
Kaisei Waki <me@kaisei.dev>
2021-10-05 21:17:50 +0900

    fix: pathPrefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,6 @@ module.exports = {
     siteUrl: "https://k41531.github.io/ember.theme/",
     title: "Ember Theme",
   },
-  pathPrefix: "ember.theme",
+  pathPrefix: "/ember.theme",
   plugins: ["gatsby-plugin-styled-components"],
 };
